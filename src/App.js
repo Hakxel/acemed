fb83ad263adc16e454cdc7ec814726c849a54fb3
Hakxel <asiel_montes@yahoo.com>
2019-04-23 11:03:52 -0400

    Set initial routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <article className="app">
+      <Navigation />
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+      </Router>
+      <Footer />
+    </article>
   )
 }
 
