25924ff299a80dd955bb3a73b66f5b6514016152
Hakxel <asiel_montes@yahoo.com>
2020-03-22 11:49:19 -0400

    Improve services container

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,11 +6,11 @@ function Services({t}) {
   return (
     <div id="services" className="home-component">
       <div className="p-grid p-justify-center">
-        <div className="p-col-6">
-          <h2>{t('Services')}</h2>
-        </div>
-        <div className="p-row">
-          <div className="p-col-12">
+      <div className="services-wrapper p-col-12">
+          <div className="p-grid p-justify-center">
+            <h2>{t('Services')}</h2>
+          </div>
+          <div className="p-grid p-justify-center">
             <ServiceCarousel />
           </div>
         </div>
