eb8c85e2ed9364a865fccf87d27f8dd2fdf9932d
Hakxel <asiel_montes@yahoo.com>
2020-03-22 12:36:44 -0400

    Improve about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,14 @@ function About() {
   return (
     <div id="about" className="home-component">
       <div className="p-grid p-justify-center about-container">
-        <div className="p-col-10 p-md-5 about-panel panel-left">
+        <div className="p-col-12 p-md-5 about-panel panel-left">
         </div>
         <div className="p-col-10 p-md-5 about-panel panel-right">
           <div className="p-grid">
             <div className="p-col-12 p-col-align-center">
-              <h3>About Ace Medical & Rehab</h3>
+              <h2>About Ace Medical & Rehab</h2>
               <p>Nulla velit commodo ullamco elit laboris est exercitation labore do commodo in nisi ut aliqua. Ipsum dolor nostrud in et tempor dolore commodo excepteur dolore elit non eu tempor. Veniam ullamco adipisicing sunt consequat quis in incididunt consectetur laboris ad.</p>
+              <p>Elit cillum consequat laborum anim consectetur veniam eiusmod. Exercitation nulla adipisicing consequat adipisicing. Ut nisi nisi aute non.</p>
             </div>
           </div>  
         </div>
