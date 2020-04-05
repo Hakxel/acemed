de9875a7f52d582de780388c57d4a58cb83bd4f0
Hakxel <asiel_montes@yahoo.com>
2020-04-05 09:25:01 -0400

    Improve general responsiveness

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Schedule() {
   return (
     <div id="schedule" className="home-component">
-      <div className="p-grid p-justify-center">
+      <div className="p-grid p-align-center p-justify-center schedule-wrapper">
         <div className="p-col-12 p-md-10">
           <div className="p-grid p-align-center p-justify-center schedule-info">
-            <div className="p-col-12 p-col-justify-center schedule-wrapper">
+            <div className="p-col-12 p-md-10 p-col-justify-center info-wrapper">
               <h2>Clinic Hours</h2>
               <h3>Here when you need us</h3>
-              <div className="p-grid p-justify-around p-align-center">
+              <div className="p-grid p-justify-around p-align-center hours-wrapper">
                 <div className="p-col-6 p-col-">
                   <h4>Mon - Wed - Fri</h4>
                   <p>8:00 am - 5:00 pm</p>
