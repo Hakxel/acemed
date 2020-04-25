30a1f44d196909f5e63a4cdc9578029da26dca0e
Hakxel <asiel_montes@yahoo.com>
2020-04-25 10:21:39 -0400

    Add address and telephone to location component

--- src/components/Location.js
+++ src/components/Location.js
@@ -18,6 +18,14 @@ function Location() {
   return (
     <div id="location" className="home-component">
       <div className="p-grid p-justify-center">
+        <div className="p-col-12 p-md-6">
+          <address>3990 W Flager St. Suite 102</address>
+          <address>Coral Gables</address>
+          <address>FL 33134</address>
+          <address>Tel: 305 392 1143</address>
+        </div>
+      </div>
+      <div className="p-grid p-justify-center">
         <div className="p-col-12 p-md-10 map-wrapper">
           <GMap options={mapOptions} overlays={marker} style={{width: '100%', minHeight: '550px'}}/>
         </div>
