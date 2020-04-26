a529a463d7bb6e6d496f1b8e12c7a6c0d456c30e
Hakxel <asiel_montes@yahoo.com>
2020-04-26 12:31:52 -0400

    Add real services and modify images

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -10,20 +10,20 @@ function Schedule({t}) {
               <h2>{t('Schedule', 'Header')}</h2>
               <h3>{t('Schedule', 'Subheader')}</h3>
               <div className="p-grid p-justify-around hours-wrapper">
-                <div className="p-col-12 p-md-7">
+                <div className="p-col-12 p-md-6">
                   <h4>{t('Schedule', 'Days_1')}</h4>
                   <div className="p-grid p-align-center">
-                    <div className="p-col-12 p-lg-6 schedule-hours">
+                    <div className="p-col-12 schedule-hours">
                       <span>{t('Schedule', 'Mode_1')}</span>
                       <p>9:00 am - 4:00 pm</p>
                     </div>
-                    <div className="p-col-12 p-lg-6 schedule-hours">
+                    <div className="p-col-12 schedule-hours">
                       <span>{t('Schedule', 'Mode_2')}</span>
                       <p>4:00 pm - 7:00 pm</p>
                     </div>
                   </div>
                 </div>
-                <div className="p-col-12 p-md-5">
+                <div className="p-col-12 p-md-6">
                   <h4>{t('Schedule', 'Days_2')}</h4>
                   <div className="schedule-hours">
                     <span>{t('Schedule', 'Mode_2')}</span>
