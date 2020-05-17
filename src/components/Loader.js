ed4db05c0fe7e977e04e6d8ac47de488ae09f0cd
Hakxel <asiel_montes@yahoo.com>
2020-05-17 11:49:42 -0400

    Add spinning loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {ProgressSpinner} from 'primereact/progressspinner'
 
 function Loader() {
   return (
-    <div>
-      <h3>Loading...</h3>
+    <div className="p-grid p-justify-center p-align-center loader">
+      <ProgressSpinner style={{width: '10rem', height: '10rem'}} strokeWidth="4" fill="#EEEEEE" animationDuration="2s"/>
     </div>
   )
 }
